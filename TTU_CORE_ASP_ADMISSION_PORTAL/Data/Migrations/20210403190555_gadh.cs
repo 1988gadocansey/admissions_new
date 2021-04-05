4a32d3397eb882935a0bb55f2d2137528c88c15b
1988gadocansey <gadocansey@gmail.com>
2021-04-05 17:28:07 +0000

    * AcademicExpController.cs: added editing to form
    * preview.cshtml:
    * ResultController.cs:
    * WorKExpController.cs:
    * 20210403185908_gad.cs:
    * 20210403190442_gad.cs:
    * 20210403190555_gadh.cs:
    * 20210403190049_gadm.cs:
    * 20210403190255_gadmm.cs:
    * 20210403164759_mmm2ssmm.cs:
    * 20210403164932_mmm2ssmmm.cs:
    * 20210403185908_gad.Designer.cs:
    * 20210403190442_gad.Designer.cs:
    * 20210403190555_gadh.Designer.cs:
    * 20210403190049_gadm.Designer.cs:
    * 20210403190255_gadmm.Designer.cs:
    * 20210403164759_mmm2ssmm.Designer.cs:
    * 20210403164932_mmm2ssmmm.Designer.cs:

---
+++ TTU_CORE_ASP_ADMISSION_PORTAL/Data/Migrations/20210403190555_gadh.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TTU_CORE_ASP_ADMISSION_PORTAL.Data.Migrations
+{
+    public partial class gadh : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+
+             
+
+            migrationBuilder.AlterColumn<bool>(
+               name: "ReportedInSchool",
+               table: "ApplicantModel",
+               type: "boolean",
+               nullable: true,
+               oldClrType: typeof(bool),
+               oldType: "boolean");
+
+
+            
+            migrationBuilder.CreateIndex(
+                name: "IX_ApplicantModel_HallId",
+                table: "ApplicantModel",
+                column: "HallId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ApplicantModel_AspNetUsers_ApplicationUserId",
+                table: "ApplicantModel",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ApplicantModel_HallModel_HallId",
+                table: "ApplicantModel",
+                column: "HallId",
+                principalTable: "HallModel",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+             
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ApplicantModel_HallModel_HallId",
+                table: "ApplicantModel");
+
+           
+
+            migrationBuilder.DropIndex(
+                name: "IX_ApplicantModel_HallId",
+                table: "ApplicantModel");
+
+            migrationBuilder.DropColumn(
+                name: "HallId",
+                table: "ApplicantModel");
+ 
+
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserId1",
+                table: "ApplicantModel",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApplicantModel_ApplicationUserId1",
+                table: "ApplicantModel",
+                column: "ApplicationUserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ApplicantModel_AspNetUsers_ApplicationUserId1",
+                table: "ApplicantModel",
+                column: "ApplicationUserId1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
